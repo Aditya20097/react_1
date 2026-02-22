6f7970b5fc9529f3f30fe3385a9717666d40a824
Aditya20097 <raj.aditya10997@gmail.com>
2026-02-23 00:17:46 +0530

    added func

--- src/component/AddFile.jsx
+++ src/component/AddFile.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const AddFile = () => {
+    function Add() {
+         a=10;
+         b=20;
+         sum =a+b
+         console.log(sum);
+         
+    }
   return (
     <div>AddFile</div>
   )
